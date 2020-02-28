0cbd99e20333052c3ff4f94040ac9efe8a8f7648
alekseisafin <safin.alexey91@gmail.com>
2020-02-27 22:13:09 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
+import {AuthService} from './services/auth.service'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-chat';
+	title = 'ng-chat'
+	// authenticated
+
+	constructor(public auth: AuthService) {
+		// this.authenticated = auth.authState
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UserLoginComponent} from './components/user-login/user-login.component';
+import {UserProfileComponent} from './components/user-profile/user-profile.component';
+import {AuthService} from './services/auth.service';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {RouterModule} from '@angular/router';
+import {AuthGuard} from './services/auth.guard';
+
+const config = {
+	apiKey: 'AIzaSyAiDNDNuqtF_gTmU6FXoBHcIjem0hDxVYY',
+	authDomain: 'ng-chat-d6a53.firebaseapp.com',
+	databaseURL: 'https://ng-chat-d6a53.firebaseio.com',
+	projectId: 'ng-chat-d6a53',
+	storageBucket: 'gs://ng-chat-d6a53.appspot.com/',
+	messagingSenderId: '535595157361'
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		UserLoginComponent,
+		UserProfileComponent
+	],
+	imports: [
+		BrowserModule,
+		AngularFireModule.initializeApp(config),
+		AngularFirestoreModule, // firestore
+		AngularFireAuthModule, // auth
+		AngularFireStorageModule, // storage,,
+		AngularFireDatabaseModule,
+		RouterModule.forRoot([
+			{ path: 'user', component: UserProfileComponent, canActivate: [AuthGuard]}
+		])
+	],
+	providers: [AuthService],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
